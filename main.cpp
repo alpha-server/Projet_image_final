90dacd7614fce0184e53789a03ef5d445b9f854c
baillema <mrmarthieu@gmail.com>
2016-01-27 13:27:23 +0100

    Ajout d'un logo.png

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("Mini Photoshop");
+    w.setWindowIcon(QIcon("/home/b/baillema/Desktop/alpha-server-Projet_image_final/logo.png"));
     w.show();
 
     return a.exec();
